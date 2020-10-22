cc49e57ca63c083b0fdf4c22fe90773ee1dffa53
Rohit Jaat <shivjindal78@gmail.com>
2020-10-22 09:14:29 +0530

    all projects

---
+++ Project/shopping_web/store/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+from .views import index, signup, login, cart, checkout, orders
+from .middlewares.auth import auth_middleware
+
+urlpatterns = [
+    path('',index.Index.as_view(), name = 'index'),
+    path('signup/',signup.Signup.as_view(), name = 'signup'),
+    path('login/',login.Login.as_view(), name = 'login'),
+    path('logout',login.logout, name = 'logout'),
+    path('cart',auth_middleware(cart.Cart.as_view()), name = 'cart'),
+    path('check-out',checkout.CheckOut.as_view(), name = 'checkout'),
+    path('orders',auth_middleware(orders.OrderView.as_view()), name = 'orders'),
+]
