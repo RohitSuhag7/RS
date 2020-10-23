012344d148d778d22f6850bea95554d3cdb85997
Rohit Jaat <shivjindal78@gmail.com>
2020-10-23 15:53:48 +0530

    projectsrmove

--- Project/Auth_Map_PaymentGateway/m4/maps/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def gmap_map(request):
-    #mapbox_access_Token = 'pk.eyJ1Ijoic2hpdmppbmRhbDc4IiwiYSI6ImNrZHBkajNhNTBoMXgycW8yYnJ6emsxMjYifQ.ebwYa-XBAX5HqTfmMOilVg'
-
-    return render(request, "gmap.html", {})
-#'mapbox_access_token' : mapbox_access_token
--- Project/Sentimental_Analysis_with_Twitter_Data/tweet/tweetstream/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TweetstreamConfig(AppConfig):
-    name = 'tweetstream'
